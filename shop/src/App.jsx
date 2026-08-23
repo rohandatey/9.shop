a89d5dfdeb44e85695d516e239e35220bf08d2b3
rohandatey <rohandatey@gmail.com>
2026-08-23 12:38:32 +0530

    first commit

---
+++ shop/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import Card from "./Pages/Card";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  return (
+    <>
+      <Navbar setProducts={setProducts} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home products={products} setProducts={setProducts} />}
+        />
+        <Route path="/card" element={<Card />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ shop/src/Pages/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>This is a Card page</div>
+  )
+}
+
+export default Card
